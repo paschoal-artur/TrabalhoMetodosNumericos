e27ac82b95ba454b4e5fa343597d6b6cbc8079be
arturp <paschoal.artur@alu.ufc.br>
2024-08-04 21:02:39 -0300

    codigo atualizado

--- func_main.py
+++ func_main.py
@@ -1,12 +1,10 @@
 import numpy as np
 
 def f(Q, C):
-    return C*np.exp(Q) - 4*Q**2 
+    try:
+        return C*np.exp(Q) - 4*Q**2
+    except OverflowError:
+        return float('inf') 
 
 def df(Q, C):
-    ''' Parameters
-    Q: Valor atual de Q
-    C: Constante utilizada na função `f`
-    '''
-    #! Derivada da função `f(Q)` em relação a Q
     return C * np.exp(Q) - 8 * Q
